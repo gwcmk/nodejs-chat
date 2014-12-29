723fbe5ef0ce0ea34326fda0fbb14597dc723eb9
gwcmk <gregwcmkim@gmail.com>
2014-12-29 16:39:00 -0500

    Added integration with MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
 var path = require('path');
+var mongo = require('mongodb').MongoClient
+var client = require('socket.io').listen(server).sockets
+
+server.listen(8080, function(){
+  console.log('listening on port 8080');
+});
 
 app.use(express.static(path.join(__dirname, '')));
 
@@ -10,18 +15,50 @@ app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-  	io.emit('chat message', msg);
-    console.log('message: ' + msg);
-  });
-});
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
+	if (err)
+		throw err;
 
-http.listen(3000, function(){
-  console.log('listening on port 3000');
-});
+	client.on('connection', function(socket){
+		var col = db.collection('messages');
+		var sendStatus = function(s){
+			socket.emit('status', s);
+		}
+
+		col.find().sort({$natural:1}).limit(50).toArray(function(err, res){
+			if(err)
+				throw err;
+
+			socket.emit('chat-output', res);
+		})
+
+		socket.on('chat-input', function(data){
+			var name = data.name;
+			var message = data.message;
+			var whitespaceRegExp = /^\s*$/;
+
+			if(whitespaceRegExp.test(name) || whitespaceRegExp.test(message)){
+				sendStatus({
+						message: 'Message not sent',
+						clear: false
+					});
+			}else{
+				col.insert({name: name, message: message}, function(){
+					client.emit('chat-output', [data]);
+					sendStatus({
+						message: 'Message sent',
+						clear: true
+					});
+				})
+			}
+
+			
+		})
+	})
+})
+
+
+
+
+
 
