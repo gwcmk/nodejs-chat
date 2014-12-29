cd5942412c59dc70175f069c30080078e3c3c381
gwcmk <gregwcmkim@gmail.com>
2014-12-29 01:27:33 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+  	io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
+
